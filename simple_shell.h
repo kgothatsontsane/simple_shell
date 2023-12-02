3604cd1ffb6f98cd8d6aa7a55c5004e58bc9c786
Kgothatso <nvisionfactory@gmail.com>
2023-12-02 10:54:44 -0800

    fixing simple-shell.c Betty errors

--- simple_shell.h
+++ simple_shell.h
@@ -95,7 +95,8 @@ typedef struct passdata
 
 #define DATA_INIT \
 	{ \
-		NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		NULL, NULL, NULL, 0, 0, 0, 0, NULL, \
+		NULL, NULL, NULL, NULL, 0, 0, NULL, \
 		0, 0, 0 \
 	}
 
